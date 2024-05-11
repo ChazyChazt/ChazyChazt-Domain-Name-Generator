4390eb3c288af44af62d589345ce6154bfc228e3
ChazyChazt <147471127+ChazyChazt@users.noreply.github.com>
2024-05-11 23:12:24 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["awesome", "nice", "fantastic", "cool", "happy"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [".com", ".es", ".au", ".org", ".dev", ".nz"];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          let domain = article + adjective + noun + tld;
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
